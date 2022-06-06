1643cb635a2ff5ae81709f605c73a79e182c7843
Nam Nguyen <namnguyen.8a@gmail.com>
2022-06-06 10:24:13 +0700

    Add dependencies to the path operator decorator demo

---
+++ Depend-in-path-op-decorators.py
@@ -0,0 +1,17 @@
+# Add dependencies to the path operator decorator it should be a list of Depends():
+from fastapi import Depends, FastAPI, Header, HTTPException
+
+app = FastAPI()
+
+async def verify_token(x_token: str = Header()):
+    if x_token != "fake-super-secret-token":
+        raise HTTPException(status_code=400, detail="X-Token header invalid")
+
+async def verify_key(x_key: str = Header()):
+    if x_key != "fake-super-secret-key":
+        raise HTTPException(status_code=404, detail="X-Key header invalid")
+    return x_key
+
+@app.get("/items/", dependencies=[Depends(verify_token), Depends(verify_key)])
+async def read_items():
+    return [{"item": "Foo"}, {"item": "Bar"}]
